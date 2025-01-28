cf3f0f1272711c1c279e667d8216e410fb49bc97
airick10 <58576837+airick10@users.noreply.github.com>
2025-01-27 18:46:04 -0600

    Adjusted main file
    Calling wrestler json file
    Adding wrestler objects as a dictionary from the json

--- mainevent.py
+++ mainevent.py
@@ -3,25 +3,40 @@ import random
 import match
 import wrestlers
 import matchClass
+import json
 
+with open('wrestlerRoster.json', 'r') as file:
+    wrestler_data = json.load(file)
+
+# Create Wrestler objects and store them in a dictionary
 wrestledict = {
-    "under": wrestlers.Wrestler("The Undertaker", 88, 55, "Powerhouse", "Tombstone Piledriver", "Grapple"),
-    "rock": wrestlers.Wrestler("The Rock", 87, 60, "All-Around", "Rock Bottom", "Standing"),
-    "austin": wrestlers.Wrestler("Steve Austin", 92, 85, "Brawler", "Stone Cold Stunner", "Standing"),
-    "hbk": wrestlers.Wrestler("Shawn Michaels", 90, 40, "All-Around", "Sweet Chin Music", "Standing"),
-    "brock": wrestlers.Wrestler("Brock Lesnar", 85, 53, "All-Around", "F5", "Grapple"),
-    "jake": wrestlers.Wrestler("Jake Roberts", 78, 65, "Brawler", "DDT", "Standing")
+    key: wrestlers.Wrestler(
+        name=value["name"],
+        overall=value["overall"],
+        height=value["height"],
+        weight=value["weight"],
+        music=value["music"],
+        entrance=value["entrance"],
+        catchphrase=value["catchphrase"],
+        tauntstyle=value["tauntstyle"],
+        hometown=value["hometown"],
+        align=value["align"],
+        mic_skill=value["mic_skill"],
+        style=value["style"],
+        signature=value["signature"],
+        signaturetype=value["signaturetype"],
+        finisher=value["finisher"],
+        finishertype=value["finishertype"]
+    )
+    for key, value in wrestler_data.items()
 }
 
-print("Wrestler Dictionary Contents:")
-for key, wrestler in wrestledict.items():
-    print(f"{key}: {wrestler}")
 
 print("Welcome to Main Event!")
 winner, loser = random.sample(list(wrestledict.values()), 2)
 oneonone = matchClass.Match(winner, loser, 20, False)
+oneonone.load_data('matchStrings.json')
 oneonone.matchDirector()
-#match.matchDirector(winner, loser, 10, True)
-print("End")
+
 
 
